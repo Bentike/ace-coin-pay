1de9b87b566bc24bcb06355d5fe95d0b5ccf0210
bentike <ajadiyusuf73@gmail.com>
2023-05-25 16:10:19 +0100

    fixed input width

--- src/components/CvvNumber.js
+++ src/components/CvvNumber.js
@@ -4,12 +4,10 @@ import Header from "./Header";
 const CvvNumber = () => {
   return (
     <div className="cvv-wrap">
-      <div>
       <Header
         title="CVV Number"
         text="Enter the 3 or 4 digit number on the card"
       />
-      </div>
       <div>
         <input className="cvv-input" type="text" placeholder="327" />
       </div>
