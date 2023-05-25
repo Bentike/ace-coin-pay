2625fed7ea669850b1e79385f0238b11a8bd7389
bentike <ajadiyusuf73@gmail.com>
2023-05-25 13:13:39 +0100

    added PriceTag

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -2,6 +2,7 @@ import CardItem from "./CardItem";
 import CompanyCard from "./CompanyCard";
 import { FaApple } from 'react-icons/fa';
 import "../styles/cardDetail.css";
+import PriceTag from "./PriceTag";
 const CardDetail = () => {
 
     return (
@@ -10,6 +11,8 @@ const CardDetail = () => {
             <CardItem item="Order Number" value="1266201"/>
             <CardItem item="Product" value="MackBook Air"/>
             <CardItem item="VAT(20%)" value="$100.00"/>
+            <hr/>
+            <PriceTag/>
         </div>
     );
 }
---
+++ src/components/PriceTag.js
@@ -0,0 +1,15 @@
+import "../styles/priceTag.css";
+
+const PriceTag = () => {
+    return(
+        <div>
+            <div>
+                <p>You have to Pay</p>
+                <p>549.99 USD</p>
+            </div>
+            <div></div>
+        </div>
+    );
+}
+
+export default PriceTag;
