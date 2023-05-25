6f7591ad5150b3227aa4e7aa3f7c561c4b2dde60
bentike <ajadiyusuf73@gmail.com>
2023-05-25 18:44:16 +0100

    added grid icon to password input

--- src/components/Password.js
+++ src/components/Password.js
@@ -1,11 +1,14 @@
 import Header from "./Header";
 import "../styles/password.css";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+
 
 const Password = () => {
   return (
     <div className="cvv-wrap">
      <Header title="Password" text="Enter your Dynamic pasword"/>
-      <div>
+      <div className="password-grid">
+        <BsFillGrid3X3GapFill className="password-grid-icon"/>
         <input className="password-input" type="password" placeholder="5585964940" />
       </div>
     </div>
